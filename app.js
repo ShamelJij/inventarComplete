666a5e9c922285d1cc0a5cc8d02a3644f5ce9c4c
shameljij <shamel.jij@gmail.com>
2021-11-17 12:17:30 +0100

    functional version

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var selectedRow = null;
 function onFormSubmit(e){
     event.preventDefault();
@@ -30,23 +29,23 @@ function insertNewRecord(data){
     var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     var cell1 = newRow.insertCell(0);
-        cell1.innerHTML = data.statusid;
+    cell1.innerHTML = data.statusid;
     var cell2 = newRow.insertCell(1);
-        cell2.innerHTML = data.anschaffVonid;
+    cell2.innerHTML = data.anschaffVonid;
     var cell3 = newRow.insertCell(2);
-        cell3.innerHTML = data.anschaffBisid;
+    cell3.innerHTML = data.anschaffBisid;
     var cell4 = newRow.insertCell(3);
-        cell4.innerHTML = data.inventarBezeichnungid;
+    cell4.innerHTML = data.inventarBezeichnungid;
     var cell5 = newRow.insertCell(4);
-        cell5.innerHTML = data.seriennummerid;
+    cell5.innerHTML = data.seriennummerid;
     var cell6 = newRow.insertCell(5);
-        cell6.innerHTML = data.preisnettoid;
+    cell6.innerHTML = data.preisnettoid;
     var cell7 = newRow.insertCell(6);
-        cell7.innerHTML = data.abschreibungZeitraumVonid;
+    cell7.innerHTML = data.abschreibungZeitraumVonid;
     var cell8 = newRow.insertCell(7);
-        cell8.innerHTML = data.abschreibungZeitraumBisid;
+    cell8.innerHTML = data.abschreibungZeitraumBisid;
     var cell9 = newRow.insertCell(8);
-        cell9.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
+    cell9.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
 }
 
 
@@ -94,127 +93,3 @@ function resetForm(){
     document.getElementById('abschreibungZeitraumVonid').value = '';
     document.getElementById('abschreibungZeitraumBisid').value = '';
 }
-
-
-
-
-
-
-
-
-
-
-/*function insertNewInventar(inventarData){
-    var table = document.getElementById("inventarTabelle").getElementsByTagName("tbody")[0];
-    var inventarNewRaw = table.insertRow(table.length);
-    inventarCell1 = inventarNewRaw.insertCell(0);
-    inventarCell1.innerHTML = inventarData.status;
-    inventarCell2 = inventarNewRaw.insertCell(1);
-    inventarCell2.innerHTML = inventarData.anschaffVonid;
-    inventarCell3 = inventarNewRaw.insertCell(2);
-    inventarCell3.innerHTML = inventarData.anschaffBisid;
-    inventarCell4 = inventarNewRaw.insertCell(3);
-    inventarCell4.innerHTML = inventarData.inventarBezeichnungid;
-    inventarCell5 = inventarNewRaw.insertCell(4);
-    inventarCell5.innerHTML = inventarData.seriennummerid;
-    inventarCell6 = inventarNewRaw.insertCell(5);
-    inventarCell6.innerHTML = inventarData.preisnettoid;
-    inventarCell7 = inventarNewRaw.insertCell(6);
-    inventarCell7.innerHTML = inventarData.abschreibungZeitraumVonid;
-    inventarCell8 = inventarNewRaw.insertCell(7);
-    inventarCell8.innerHTML = inventarData.abschreibungZeitraumBisid;
-    inventarCell9 = inventarNewRaw.insertCell(8);
-    inventarCell9.innerHTML = "<button>Edit</button>";
-    inventarCell10 = inventarNewRaw.insertCell(9);
-    inventarCell10.innerHTML = "<button>Delete</button>"
-}*/
-
-
-
-
-
-
-
-
-
-
-/*function inventarSpeicher(){
-    var inventarFormData = inventarReadFormData();
-    insertNewInventar(inventarFormData);  
-}
-
-function inventarReadFormData(){
-    var inventarFormData = {};
-    inventarFormData ["statusid"] = document.getElementById("statusid").value;
-    inventarFormData ["anschaffVonid"] = document.getElementById("anschaffVonid").value;
-    inventarFormData ["anschaffBisid"] = document.getElementById("anschaffBisid").value;
-    inventarFormData ["inventarBezeichnungid"] = document.getElementById("inventarBezeichnungid").value;
-    inventarFormData ["seriennummerid"] = document.getElementById("seriennummerid").value;
-    inventarFormData ["preisnettoid"] = document.getElementById("preisnettoid").value;
-    inventarFormData ["abschreibungZeitraumVonid"] = document.getElementById("abschreibungZeitraumVonid").value;
-    inventarFormData ["abschreibungZeitraumBisid"] = document.getElementById("abschreibungZeitraumBisid").value;
-    return inventarFormData;
-}
-
-function insertNewInventar(inventarData){
-    var table = document.getElementById("inventarTabelle").getElementsByTagName("tbody")[0];
-
-    var inventarNewRaw = table.insertRow(table.length);
-    inventarCell1 = inventarNewRaw.insertCell(0);
-    inventarCell1.innerHTML = inventarData.status;
-    inventarCell2 = inventarNewRaw.insertCell(1);
-    inventarCell2.innerHTML = inventarData.anschaffVonid;
-    inventarCell3 = inventarNewRaw.insertCell(2);
-    inventarCell3.innerHTML = inventarData.anschaffBisid;
-    inventarCell4 = inventarNewRaw.insertCell(3);
-    inventarCell4.innerHTML = inventarData.inventarBezeichnungid;
-    inventarCell5 = inventarNewRaw.insertCell(4);
-    inventarCell5.innerHTML = inventarData.seriennummerid;
-    inventarCell6 = inventarNewRaw.insertCell(5);
-    inventarCell6.innerHTML = inventarData.preisnettoid;
-    inventarCell7 = inventarNewRaw.insertCell(6);
-    inventarCell7.innerHTML = inventarData.abschreibungZeitraumVonid;
-    inventarCell8 = inventarNewRaw.insertCell(7);
-    inventarCell8.innerHTML = inventarData.abschreibungZeitraumBisid;
-    inventarCell9 = inventarNewRaw.insertCell(8);
-    inventarCell9.innerHTML = "<button>Edit</button>";
-    inventarCell10 = inventarNewRaw.insertCell(9);
-    inventarCell10.innerHTML = "<button>Delete</button>"
-}*/
-
-/*
-function inventarSpeicher(){
-    var new_inventarSpeicher = document.getElementById('inventar-speicher').value;
-    if(localStorage.getItem('inventarSpeicher') == null){
-        localStorage.setItem('inventarSpeicher','[]');
-
-    }
-    var old_inventarSpeicher = JSON.parse(localStorage.getItem('inventarSpeicher'));
-    old_inventarSpeicher.push(new_inventarSpeicher);
-    localStorage.setItem('inventarSpeicher', JSON.stringify(old_inventarSpeicher));
-}
-function inventarTabelle(){
-    if (localStorage.getItem('inventarSpeicher')!=null){
-        document.getElementById('inventarZeigen').innerHTML = JSON.parse(localStorage.getItem('inventarSpeicher'));
-    }
-}
-
-
-
-
-function save(){
-
-    var new_data = document.getElementById('input').value;
-    if(localStorage.getItem('data') == null){
-        localStorage.setItem('data','[]');
-
-    }
-    var old_data = JSON.parse(localStorage.getItem('data'));
-    old_data.push(new_data);
-    localStorage.setItem('data', JSON.stringify(old_data));
-}
-    function view(){
-        if (localStorage.getItem('data')!=null){
-            document.getElementById('output').innerHTML = JSON.parse(localStorage.getItem('data'));
-        }
-}*/
