27d3a6d99b118ab3b7ba6f0c1e2bc90404046daa
shameljij <shamel.jij@gmail.com>
2021-11-17 15:24:38 +0100

    end result

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+setInterval(createSnowFlake,50);
+setInterval(createStars,5);
+function createSnowFlake() {
+    const snow_flake = document.createElement('b');
+    snow_flake.textContent = "❄";
+    snow_flake.classList.add('snowcla');
+    snow_flake.classList.add('snow-flake');
+    snow_flake.style.left = Math.random() * window.innerWidth + 'px';
+    snow_flake.style.animationDuration = Math.random() * 3 + 2 + 's';
+    snow_flake.style.opacity = Math.random();
+    snow_flake.style.fontSize = Math.random() * 10 + 10 + 'px';
+    document.getElementById('snow').appendChild(snow_flake);
+    setTimeout(() => {
+        snow_flake.remove();
+    }, 12000)
+}
+function createStars(){
+    const stars= document.createElement('b')
+    stars.textContent=('✹');
+    stars.classList.add('glowcla');
+    stars.classList.add('glow');
+    stars.style.left = Math.random() * window.innerWidth +'px';
+    stars.style.bottom = Math.random() * window.innerHeight +'px';
+    stars.style.opacity = Math.random();
+    stars.style.fontSize= Math.random() * 10 + 10 + 'px';
+    document.getElementById('snow').appendChild(stars);
+    setTimeout(() => {
+        stars.remove();
+    }, 4000)
+}
+
+
+
+
 var selectedRow = null;
 function onFormSubmit(e){
     event.preventDefault();
@@ -60,11 +94,7 @@ function onEdit(td){
     document.getElementById('preisnettoid').value = selectedRow.cells[5].innerHTML;
     document.getElementById('abschreibungZeitraumVonid').value = selectedRow.cells[6].innerHTML;
     document.getElementById('abschreibungZeitraumBisid').value = selectedRow.cells[7].innerHTML;
-    function calcDatum{
-    var datumone = document.getElementById('abschreibungZeitraumVonid').value;
-    var datumtwo = document.getElementById('abschreibungZeitraumBisid').value;
-    }
-    //comment
+
 }
 
 function updateRecord(formData){
--- test files/app.js
+++ test files/app.js
@@ -94,7 +94,22 @@ function resetForm(){
     document.getElementById('abschreibungZeitraumVonid').value = '';
     document.getElementById('abschreibungZeitraumBisid').value = '';
 }
+function save(){
 
+    var new_data = document.getElementById('input').value;
+    if(localStorage.getItem('data') == null){
+        localStorage.setItem('data','[]');
+
+    }
+    var old_data = JSON.parse(localStorage.getItem('data'));
+    old_data.push(new_data);
+    localStorage.setItem('data', JSON.stringify(old_data));
+}
+function view(){
+    if (localStorage.getItem('data')!=null){
+        document.getElementById('output').innerHTML = JSON.parse(localStorage.getItem('data'));
+    }
+}*/
 
 
 
