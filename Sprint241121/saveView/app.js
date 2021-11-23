2992269f1cc1c8a658f21b26deb71393712e0c20
shameljij <shamel.jij@gmail.com>
2021-11-23 02:51:42 +0100

    single page function added

--- Sprint241121/saveView/app.js
+++ Sprint241121/saveView/app.js
@@ -1,3 +1,28 @@
+window.onload = () => {
+    const tab_switchers = document.querySelectorAll('[data-switcher]');
+
+    for (let i = 0; i < tab_switchers.length; i++) {
+        const tab_switcher = tab_switchers[i];
+        const page_id = tab_switcher.dataset.tab;
+
+        tab_switcher.addEventListener('click', () => {
+            document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
+            tab_switcher.parentNode.classList.add('is-active');
+
+            SwitchPage(page_id);
+        });
+    }
+}
+
+function SwitchPage (page_id) {
+    console.log(page_id);
+
+    const current_page = document.querySelector('.pages .page.is-active');
+    current_page.classList.remove('is-active');
+
+    const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+    next_page.classList.add('is-active');
+}
 function save(){
 
     var new_data = document.getElementById('input').value;
@@ -13,4 +38,31 @@ function save(){
         if (localStorage.getItem('data')!=null){
             document.getElementById('output').innerHTML = JSON.parse(localStorage.getItem('data'));
         }
+}
+var myArray = [
+    {'name':'Michael', 'age':'30', 'birthdate':'11/10/1989'},
+    {'name':'Mila', 'age':'32', 'birthdate':'10/1/1989'},
+    {'name':'Paul', 'age':'29', 'birthdate':'10/14/1990'},
+    {'name':'Dennis', 'age':'25', 'birthdate':'11/29/1993'},
+    {'name':'Tim', 'age':'27', 'birthdate':'3/12/1991'},
+    {'name':'Erik', 'age':'24', 'birthdate':'10/31/1995'},
+]
+
+buildTable(myArray);
+
+
+
+function buildTable(data){
+    var table = document.getElementById('myTable')
+
+    for (var i = 0; i < data.length; i++){
+        var row = `<tr>
+                        <td>${data[i].name}</td>
+                        <td>${data[i].age}</td>
+                        <td>${data[i].birthdate}</td>
+                  </tr>`
+        table.innerHTML += row
+
+
+    }
 }
