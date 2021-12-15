106408a9fe7b59ef4e86a52f865fcd3fb4b62c67
shameljij <shamel.jij@gmail.com>
2021-12-15 14:03:54 +0100

    validation implementation is working too!!

--- app.js
+++ app.js
@@ -1,32 +1,9 @@
 
-
-function test(){
-    let x = document.getElementById("idPurchaseDate").className;
-    x = x.replace('is-invalid','');
-    x = x.replace('is-valid','');
-    x = x.trim();
-    document.getElementById("idPurchaseDate").className = x + " is-invalid";
-    alert( document.getElementById("idPurchaseDate").className);
-    document.getElementById("idPurchaseDateInvalid").innerText = "Das Datum legt in Zukunft!";
-
-}
-function testSuccess (){
-    let y = document.getElementById("idPurchaseDate").className;
-    y = y.replace('is-invalid','');
-    y = y.replace('is-valid','');
-    y = y.trim();
-    document.getElementById("idPurchaseDate").className = y + " is-valid";
-    alert( document.getElementById("idPurchaseDate").className);
-    document.getElementById("idPurchaseDateValid").innerText = "Valid is aktiv!";
-
-}
-
-
 //check all input validation
 function inputValidation() {
     //Anschaffungsdatum validieren (muss nicht in zukunft sein)
     //Anschaffungsdatum als wert
-    let purchaseDate = document.getElementById("validationPurchaseDate").value;
+    let purchaseDate = document.getElementById("idPurchaseDate").value;
     //jetztgen Datum
     let nowDate = new Date().toISOString().split('T')[0];
 
@@ -35,9 +12,50 @@ function inputValidation() {
 
     if (purchaseDate > nowDate){
         console.log('purchase date is bigger than now');
+        let x = document.getElementById("idPurchaseDate").className;
+        x = x.replace('is-invalid','');
+        x = x.replace('is-valid','');
+        x = x.trim();
+        document.getElementById("idPurchaseDate").className = x + " is-invalid";
+        document.getElementById("idPurchaseDateInvalid").innerText = "Das Datum legt in Zukunft!";
+    }
+    else if (purchaseDate == ''){
+        let t = document.getElementById("idPurchaseDate").className;
+        t = t.replace('is-invalid','');
+        t = t.replace('is-valid','');
+        t = t.trim();
+        document.getElementById("idPurchaseDate").className = t + " is-invalid";
+        document.getElementById("idPurchaseDateInvalid").innerText = "Das Datum ist leer!";
     }
     else {
         console.log('purchase date is smaller than now');
+        let y = document.getElementById("idPurchaseDate").className;
+        y = y.replace('is-invalid','');
+        y = y.replace('is-valid','');
+        y = y.trim();
+        document.getElementById("idPurchaseDate").className = y + " is-valid";
+        document.getElementById("idPurchaseDateValid").innerText = "Das Datum ist gültig!";
+    }
+    //price validation muss nicht negatives Wert haben
+    let price = document.getElementById('validationPrice').value;
+    //Show booking category
+    if (price < 0){
+        console.log('price is not negative');
+        let x = document.getElementById("validationPrice").className;
+        x = x.replace('is-invalid','');
+        x = x.replace('is-valid','');
+        x = x.trim();
+        document.getElementById("validationPrice").className = x + " is-invalid";
+        document.getElementById("validationPriceInvalid").innerText = "Kein negativem Wert bitte!";
+    }
+    else {
+        console.log('price is negative');
+        let y = document.getElementById("validationPrice").className;
+        y = y.replace('is-invalid','');
+        y = y.replace('is-valid','');
+        y = y.trim();
+        document.getElementById("validationPrice").className = y + " is-valid";
+        document.getElementById("validationPriceValid").innerText = "Der Wert ist gültig!";
     }
 }
 
@@ -46,7 +64,7 @@ function calcForm(){
     try {
 
 
-        let purchaseDate = document.getElementById("validationPurchaseDate").value;
+        let purchaseDate = document.getElementById("idPurchaseDate").value;
         //let getMonth = new Date(purchaseDate);
         let inputMonthValue = parseInt(document.getElementById("depreciationInput").value);
         let d = new Date(purchaseDate);
@@ -120,9 +138,12 @@ function refreshForm(){
     //price value to changes div (bookingCategory)
     let price = document.getElementById('validationPrice').value;
     //Show booking category
-    if (price <= 2000 && price >= 0){
+    if (price <= 2000 && price > 0){
         console.log('pricevis');
 
+        document.getElementById("rowDepreciation").className = 'd-none';
+    }
+    else if (price <= 0 ){
         document.getElementById("rowDepreciation").className = 'd-none';
     }
     else {
