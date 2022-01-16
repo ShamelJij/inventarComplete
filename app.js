5b88358a4b63bb1ff949766a97250c3328abff0a
shameljij <shamel.jij@gmail.com>
2022-01-16 18:52:44 +0100

    added js

---
+++ app.js
@@ -0,0 +1,38 @@
+
+let min = 1;
+let max = 100;
+
+function getRanNum1(min, max) {
+    
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let randomNumber1 = Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    document.getElementById('randomNum1').innerText = randomNumber1;
+    return randomNumber1; 
+  } 
+   
+
+  function getRanNum2(min, max) {
+    
+    
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let randomNumber2 = Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    document.getElementById('randomNum2').innerText = randomNumber2;
+    return randomNumber2; 
+  } 
+
+  function percent(num1,num2) {
+      num1 = getRanNum1(min, max);
+      num2 = getRanNum2(min, max);
+        
+    let result = num2 * num1 /100;
+        return result;
+
+  }
+
+   function showNum(){
+        document.getElementById('showResult').innerText = percent(getRanNum1(min, max),getRanNum2(min, max)) ;
+        
+
+   }
