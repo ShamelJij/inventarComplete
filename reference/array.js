12d8e0d8b7db28691b48ef75afb4ab977a2e1018
shameljij <shamel.jij@gmail.com>
2022-01-31 17:43:09 +0100

    before logging into full-stack

--- reference/array.js
+++ reference/array.js
@@ -1,7 +1,11 @@
 let l_name = "michael";
-let letters = /^[a-zA-Z]*$/;
+let letters = /^[a-zA-ZäöüÄÖÜß]*$/;
 if (l_name == '' || !letters.test(l_name)){
     console.log('not working');
 }else{
     console.log('working');
+}
+let a = [[2,4],[3,5],[4,6],[5,7],[6,8],[7,9],[8,10],[9,11],[10,12],[11,13]];
+for (var i = 0, j = 9; i <= 9; i++, j--) {
+    console.log('a[' + i + '][' + j + '] = ' + a[i][j]);
 }
