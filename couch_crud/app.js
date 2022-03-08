7fc33f42981b0db3d78b108cb71effab4818d3e6
shameljij <shamel.jij@gmail.com>
2022-03-08 11:40:42 +0100

    started Async to exercise async wait keywords. started couch_crud under folder crouch_crud to build a crdu application to exercise with crud and couchDB

---
+++ couch_crud/app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const NodeCouchDb = require('node-couchdb');
+
+const couch = new NodeCouchDb({
+    auth: {
+        user:'admin',
+        password: '123456'
+    }
+});
+
+const dbName = 'mytest';
+const viewUrl = '_design/location/_view/location-view';
+
+couch.listDatabases().then(function (dbs){
+    console.log(dbs);
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function (req, res){
+    couch.get(dbName, viewUrl).then(
+        function (data, headers, status){
+            console.log(data.data.rows);
+            res.render('index', {
+                mytest:data.data.rows
+            });
+        },
+        function (err){
+            res.send(err);
+        });
+});
+app.post('/location/add',function (req,res){
+   const locationname = req.body.locationname;
+   const locationlabel = req.body.locationlabel;
+   const locationstreet = req.body.locationstreet;
+   const housenumber = req.body.housenumber;
+   const zipcode = req.body.zipcode;
+   const floornumber = req.body.floornumber;
+   const roomnumber = req.body.roomnumber;
+
+   couch.uniqid().then(function (ids){
+       const id = ids[0];
+
+       couch.insert('mytest', {
+           _id: id,
+           locationname: locationname,
+           locationlabel: locationlabel,
+           locationstreet: locationstreet,
+           housenumber: housenumber,
+           zipcode: zipcode,
+           floornumber: floornumber,
+           roomnumber: roomnumber
+       }).then(
+           function (data, headers, status){
+                res.redirect('/');
+           },
+           function (err){
+               res.send(err);
+           });
+   });
+});
+app.listen(3000, function (){
+    console.log('server started on Port 3000');
+
+});
+
