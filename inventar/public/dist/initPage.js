19fa2e6c21c3cc7c0b62e04d5a4dc69c9ad4c766
shamel jij <shamel.jij@gmail.com>
2022-08-12 15:46:07 +0200

    have to add if to insertNewRecord

--- inventar/public/dist/initPage.js
+++ inventar/public/dist/initPage.js
@@ -5,6 +5,38 @@ class InitPage{
         this.tableId = tableId;
     }
 
+    //--------------------------------------------------------------------------------
+    /**
+     * inserts new record into table below form
+     *
+     * @param {Object} person
+     */
+    insertNewRecord(person){
+
+        let table = document.getElementById("persons").getElementsByTagName('tbody')[0];
+        let newRow = table.insertRow(table.length);
+        let cell1 = newRow.insertCell(0);
+        cell1.innerHTML = person.lastname;
+        let cell2 = newRow.insertCell(1);
+        cell2.innerHTML = person.firstname;
+        let cell3 = newRow.insertCell(2);
+        cell3.innerHTML = person.personalno;
+        let cell4 = newRow.insertCell(3);
+        cell4.innerHTML = person.email;
+        let cell5 = newRow.insertCell(4);
+        cell5.innerHTML = person._id;
+        let cell6 = newRow.insertCell(5);
+        cell6.innerHTML = "<div class=\"text-center d-flex justify-content-between\">" +
+                                "<button onClick=\"editPerson("  + "\'" + person._id + "\'" + ")\" class=\"btn btn-secondary fa fa-edit\" data-toggle=\"tooltip\" data-placement=\"left\" title=\"bearbeiten\"></button>&nbsp;" +
+            "<div data-toggle=\"tooltip\" data-placement=\"left\"><button   class=\"btn btn-danger fa fa-trash\" data-toggle=\"modal\"  title=\"löschen\" data-target=\"#deletePersonModel\" onClick=\"setRowId(" + "\'"  + person._id + "\'" + ")\"></button></div>" +
+            "</div>";
+    }
+
+    //################################################################################
+    /**
+     *
+     * initiate page
+     */
     async initPage(data){
     let objArray= await Req.getAll(data);
     console.log('GET:' + data + ': ', objArray);
